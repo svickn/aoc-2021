f4a49ec653ea3acc8bb478b42d24c45c239503f1
Stacy Vicknair <svickn@gmail.com>
2021-12-06 15:14:55 -0600

    added getLinesWithBitInPosition

--- src/day-03/part-02/d03p02-diagnostics-processor.js
+++ src/day-03/part-02/d03p02-diagnostics-processor.js
@@ -22,6 +22,20 @@ const getBit = async (comparator, tiebreakWinner, input, position) => {
     : '0';
 };
 
+const getLinesWithBitInPosition = async (input, position, bit) => {
+  const output = [];
+
+  for await (const line of input) {
+    console.log(line[position], bit);
+
+    if (line[position] === bit && !output.includes(line)) {
+      output.push(line);
+    }
+  }
+
+  return output;
+};
+
 const getMostProminentBit = (...args) =>
   getBit(comparator.mostCommon, '1', ...args);
 const getLeastProminentBit = (...args) =>
@@ -29,3 +43,4 @@ const getLeastProminentBit = (...args) =>
 
 exports.getMostProminentBit = getMostProminentBit;
 exports.getLeastProminentBit = getLeastProminentBit;
+exports.getLinesWithBitInPosition = getLinesWithBitInPosition;
--- src/day-03/part-02/d03p02-diagnostics-processor.test.js
+++ src/day-03/part-02/d03p02-diagnostics-processor.test.js
@@ -1,6 +1,7 @@
 const {
   getMostProminentBit,
   getLeastProminentBit,
+  getLinesWithBitInPosition,
 } = require('./d03p02-diagnostics-processor');
 
 test.each([
@@ -11,7 +12,7 @@ test.each([
   [['01010', '01010', '00010', '00010'], 1, '1'],
   [['11111', '11110', '11110', '11110'], 4, '0'],
 ])(
-  'getMostProminentBit(%p,%p,%p) returns %p',
+  'getMostProminentBit(%p,%p) returns %p',
   async (input, position, expected) => {
     const result = await getMostProminentBit(input, position);
 
@@ -27,10 +28,23 @@ test.each([
   [['11010', '11010', '10010', '10010'], 1, '0'],
   [['11111', '11110', '11110', '11110'], 4, '1'],
 ])(
-  'getLeastProminentBit(%p,%p,%p) returns %p',
+  'getLeastProminentBit(%p,%p) returns %p',
   async (input, position, expected) => {
     const result = await getLeastProminentBit(input, position);
 
     expect(result).toBe(expected);
+  },
+);
+
+test.each([
+  [['1', '1', '1', '0'], 0, '0', ['0']],
+  [['1', '1', '1', '0'], 0, '1', ['1']],
+  [['1101', '1111', '1111', '1010', '1001'], 3, '1', ['1101', '1111', '1001']],
+])(
+  'getLinesWithBitInPosition(%p,%p,%p) returns %p',
+  async (input, position, bit, expected) => {
+    const result = await getLinesWithBitInPosition(input, position, bit);
+
+    expect(result).toStrictEqual(expected);
   },
 );
